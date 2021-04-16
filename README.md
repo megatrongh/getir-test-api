fa2bf3da26946eb2c8bd4659caa27a7043052d0a
Klogo Gibson Selasi <klogogibson50@gmail.com>
2021-04-16 21:05:40 +0000

    package json and tests update

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # getir-test-api
 
 An expressJS with typescript application to fetch filtered records stored in a mongodb database. 
+
+## How to set up locally?
+
+- Install NodeJS
+- Install NPM
+- clone the repository using `git clone https://github.com/megatrongh/getir-test-api.git`
+- change directory into getir-test-api and run;
+  - `npm i`
+  - `npm run dev`
+
+## To run tests
+
+- run `npm test`
+
+## API Docs
+`https://getir-api-server.herokuapp.com/api-docs/`
+
+
+### Sample Request Payload
+```
+POST /api/v1/records
+Content-Type: application/json
+{
+  "startDate": "2016-01-26",
+  "endDate": "2018-02-02",
+  "minCount": 2700,
+  "maxCount": 3000
+}
+```
+### Sample Response Payload
+```
+{
+  "code": 0,
+  "msg": "Success",
+  "records": [
+    {
+      "key": "jOjBYTLV",
+      "createdAt": "2016-11-13T19:54:23.677Z",
+      "totalCount": 2954
+    },
+    {
+      "key": "fEWwrjug",
+      "createdAt": "2016-10-30T22:49:27.236Z",
+      "totalCount": 2935
+    },
+    {
+      "key": "plSuXweN",
+      "createdAt": "2016-10-25T11:36:28.069Z",
+      "totalCount": 2970
+    }
+  ]
+}
--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -12,4 +12,42 @@ describe('POST', () => {
     expect(response.status).toBe(404);
     done();
   });
+
+  it('should return a validation error if no values are sent', async (done) => {
+    const response = await request(app).post('/api/v1/records');
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toContain('ValidationError');
+    done();
+  });
+
+  it('should return a validation error if types are invalid', async (done) => {
+    const response = await request(app).post('/api/v1/records').send({
+      startDate: 3232,
+      endDate: true,
+      minCount: 'weew',
+      maxCount: false,
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toContain('ValidationError');
+    done();
+  });
+
+  it('should filter records and return a response', async (done) => {
+    const response = await request(app).post('/api/v1/records').send({
+      startDate: '2016-01-26',
+      endDate: '2018-02-02',
+      minCount: 2700,
+      maxCount: 3000,
+    });
+    const hasRecords = response?.body?.records.lenght > 0;
+    expect(response.status).toBe(200);
+    expect(response.body.code).toBe(0);
+    expect(response.body.msg).toBe('Success');
+    if (hasRecords) {
+      expect(response.body.records[0]).toHaveProperty('key');
+      expect(response.body.records[0]).toHaveProperty('createdAt');
+      expect(response.body.records[0]).toHaveProperty('totalCount');
+    }
+    done();
+  });
 });
